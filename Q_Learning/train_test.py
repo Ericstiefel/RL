fce001045b6abfa288101a38a05e0443016c40c8
Eric Stiefel <Eric.stiefel8@gmail.com>
2025-01-09 11:58:13 -0500

    update

--- Q_Learning/train_test.py
+++ Q_Learning/train_test.py
@@ -36,16 +36,3 @@ def test(map: gym.Env, agent: qAgent, testing_iter=100):
   print(f"Mean_reward={mean:.2f} +/- {sd:.2f}")
   return mean, sd
 
-# Train and test FrozenLake agent
-FROZEN_LAKE_ENV = 'FrozenLake-v1'
-fl_env = gym.make(FROZEN_LAKE_ENV, render_mode="rgb_array")
-fl_agent = qAgent()
-train(fl_env, fl_agent, solved_bound=0.95, batch_iter=30)
-test(fl_env, fl_agent)
-
-TAXI_ENV = 'Taxi-v3'
-# Train and test Taxi agent
-taxi_env = gym.make(TAXI_ENV, render_mode="rgb_array")
-taxi_agent = qAgent()
-train(taxi_env, taxi_agent, solved_bound=9.7, batch_iter=30)
-test(taxi_env, taxi_agent)
